94870b956d20c7f43725710e0607e953b8aefeb1
alomina007 <alomina007@users.noreply.github.com>
2016-07-16 07:21:14 +0430

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from telebot import types
 import requests
 import simplejson as json
 
-TOKEN = 'YOUR_BOT_TOKEN'
+TOKEN = '260214096:AAFYr32mjThh6uxW0J89MqW68_hZXzTlJ3w
 API_PATH = 'http://api.vajehyab.com/v2/public'
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(260214096:AAFYr32mjThh6uxW0J89MqW68_hZXzTlJ3w)
 
 
 @bot.message_handler(commands=['start'])
